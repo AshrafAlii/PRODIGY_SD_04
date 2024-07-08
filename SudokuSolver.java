d2d54a2c655ae77b0b5c04711ce868195424f9ea
Ashraf Ali <100949189+AshrafAlii@users.noreply.github.com>
2024-07-08 20:13:57 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,62 @@
+package Sudoko;
+
+public class SudokuSolver {
+    private int[][] board;
+    private static final int SIZE = 9;
+    private static final int EMPTY = 0;
+
+    public SudokuSolver(int[][] board) {
+        this.board = board;
+    }
+
+    public boolean solve() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == EMPTY) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solve()) {
+                                return true;
+                            } else {
+                                board[row][col] = EMPTY;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(int row, int col, int num) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int boxRowStart = (row / 3) * 3;
+        int boxColStart = (col / 3) * 3;
+
+        for (int r = boxRowStart; r < boxRowStart + 3; r++) {
+            for (int d = boxColStart; d < boxColStart + 3; d++) {
+                if (board[r][d] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void printBoard() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
